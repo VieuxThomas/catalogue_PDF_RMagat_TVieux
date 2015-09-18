ad743367a6a5be0d2161e9f12bb261d7b11c7580
VieuxThomas <thomasvieux04300@gmail.com>
2015-09-18 16:32:55 +0200

    ouverture des droit sur la base de données

--- connexion.cpp
+++ connexion.cpp
@@ -14,14 +14,14 @@ void connexion::connexionALaBase()
 {
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
     maBase=new QSqlDatabase(db);
-    maBase->setHostName("localhost");
-         maBase->setDatabaseName("NewWorld");
+    maBase->setHostName("172.16.63.111");
+         maBase->setDatabaseName("dbtvieuxNewWorld");
          maBase->setUserName("tvieux");
-         maBase->setPassword("ini01");
+         maBase->setPassword("OsjH12RRc");
          bool ok = maBase->open();
          if(!ok)
          {
-             cout<<"New World BackOffice, la connexion a la base de donnee a echouer \n Veuillez verifier que le service mysql est lancer sur localhost";
+             cout<<"New World BackOffice, la connexion a la base de donnee a echouer \n Veuillez verifier que le service mysql est lancer sur localhost"<<endl;
          }
          else
          {
