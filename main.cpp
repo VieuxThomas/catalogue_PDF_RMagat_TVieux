75b6ba2d4cc10a01ee951f033b695182a0e9f53c
rmagat <magat.info@gmail.com>
2015-09-18 16:55:23 +0200

    essai d'ajout de text dans le pdf

--- main.cpp
+++ main.cpp
@@ -16,10 +16,17 @@ int main(int argc, char *argv[])
     printer.setOutputFileName("catalogue.pdf");
     QPainter painter;
     painter.begin(&printer);
-    painter.setFont(QFont("Tahoma",8));
+    painter.setFont(QFont("Tahoma",30));
     QString nomPdf = "Catalogue New World";
     painter.drawText(300,900,nomPdf);
     painter.end();
+
+    QPainter painter2;
+    painter2.begin(&printer);
+    painter2.setFont(QFont("Tahoma",15));
+    QString resum = "Votre catalogue de produits.";
+    painter2.drawText(300,1200,resum);
+    painter2.end();
     return 0;
 }
 
