832b6723a20b149004933afbb1821c10490cac48
VieuxThomas <thomasvieux04300@gmail.com>
2015-09-18 14:55:33 +0200

    Create connexion.h

---
+++ connexion.h
@@ -0,0 +1,26 @@
+#ifndef CONNEXION_H
+#define CONNEXION_H
+
+#include <QString>
+#include <QMessageBox>
+#include <QSqlQuery>
+#include <QDebug>
+#include <QSqlRecord>
+#include <QSqlDatabase>
+#include <QSqlRelationalDelegate>
+#include <QMainWindow>
+namespace Ui {
+class MainWindow;
+}
+
+class connexion
+{
+private:
+    QSqlDatabase * maBase;
+    Ui::MainWindow *ui;
+public:
+    void connectionALaBase();
+
+};
+
+#endif // CONNEXION_H
