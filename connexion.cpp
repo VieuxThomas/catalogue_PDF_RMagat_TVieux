7e18d3202c463236e8d0644f7b00f2af97803d97
VieuxThomas <thomasvieux04300@gmail.com>
2015-09-25 14:44:01 +0200

    integration des requetes sql

--- connexion.cpp
+++ connexion.cpp
@@ -2,6 +2,7 @@
 #include <QMessageBox>
 #include <QSqlDatabase>
 #include <iostream>
+#include <QTextStream>
 
 using namespace std;
 
@@ -22,10 +23,66 @@ void connexion::connexionALaBase()
          if(!ok)
          {
              cout<<"New World BackOffice, la connexion a la base de donnee a echouer \n Veuillez verifier que le service mysql est lancer sur localhost"<<endl;
+
+
          }
          else
          {
-             cout<<"connexion etablie";
+             cout<<"connexion etablie"<<endl;
+
+             QSqlQuery maxR;
+             maxR.prepare("select max(noUtil)+1 from utilisateur");
+             maxR.exec();
+             maxR.first();
+             int max=maxR.value(0).toInt();
+
+             for (int compteur=0;compteur<max;compteur++)
+             {
+
+             QSqlQuery requeteUtilisateur;
+             requeteUtilisateur.prepare("select noUtil,nom,codePostal from utilisateur where noUtil="+QString::number(compteur)+"");
+             requeteUtilisateur.exec();
+             requeteUtilisateur.first();
+             QString noUtil=requeteUtilisateur.value(1).toString();
+             QString CP=requeteUtilisateur.value(2).toString();
+
+             QTextStream out(stdout);
+             out << QString(noUtil)<<endl;
+             out << QString(CP)<<endl;
+
+             QSqlQuery nbFav;
+             nbFav.prepare("select count(*) from QAO where noUtil="+QString::number(compteur)+"");
+             nbFav.exec();
+             nbFav.first();
+             int nbFavo=nbFav.value(0).toInt();
+
+
+             for (int compteur1=0;compteur1<nbFavo;compteur1++)
+             {
+
+                 QSqlQuery QAO;
+                 QAO.prepare("select noPDV from QAO where noUtil="+QString::number(compteur)+"");
+                 QAO.exec();
+
+                 if (compteur1==0)
+                 {
+                 QAO.first();
+                 }
+                 if  (compteur1!=0)
+                 {
+                     QAO.next();
+                 }
+                 int nb=QAO.value(0).toInt();
+
+                 cout<<nb<<endl;
+
+             }
+
+
+
+
+            }
+
          }
 
 }
